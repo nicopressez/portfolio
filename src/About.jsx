dafaeeec218b7dced35159120badd99738a96732
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-03-18 16:11:48 +0100

    Populated "about me" page

--- src/About.jsx
+++ src/About.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import aboutpic from './assets/aboutpic.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,7 +7,7 @@ const About = ({ setShowAbout, setShowResume, setShowWorks}) => {
     return (
         
         <div
-        className={`bg-gray-200 w-2/3 absolute top-1 left-1/2 h-full transform -translate-x-1/2 z-50 rounded-xl font-roobertreg text-black p-12 overflow-y-auto transition-transform
+        className={`bg-gray-200 w-2/3 absolute top-1 left-1/2 h-full transform -translate-x-1/2 z-50 rounded-xl text-black p-12 overflow-y-auto transition-transform
         
          `}
          style={{ overflowY: "scroll", scrollbarWidth: "none", msOverflowStyle: "none" }}
@@ -14,8 +15,29 @@ const About = ({ setShowAbout, setShowResume, setShowWorks}) => {
         <FontAwesomeIcon icon={faXmark} 
             className="float-right h-6 w-6 hover:cursor-pointer"
             onClick={ () => setShowAbout(false)}/>
-            <h1 className="text-4xl">About</h1>
-            
+            <h1 className="text-4xl font-roobert">About me</h1>
+            <div className='text-center font-roobert'>
+            <img src={aboutpic}
+        className=' object-cover h-80 w-80 rounded-full border-red-500 border-4 p-[0.20rem]
+         ml-auto mr-auto mb-8'/>
+
+         <p className='mb-3'>Hey there! I'm Nicolas Pressez, a self-taught full-stack web developer with a focus on front-end development and expertise in the MERN stack.</p>
+
+<p className='mb-3'>Having left my previous job in October to focus on learning web development full-time, I've dedicated the past six months to intensive study. I've been diligently going through different courses, focusing on building projects along with learning various technologies. My journey as a web developer has been fueled by a relentless pursuit of improvement, and I've poured countless hours into mastering the craft. I'm driven by a deep-seated passion for programming and a relentless determination to excel in this field.</p>
+
+<p className='mb-3'>While I'm still in the early stages of my career, I'm committed to continuous learning and growth. I thrive on challenges and embrace opportunities to push myself beyond my comfort zone. Every obstacle I encounter serves as a stepping stone for further development, and I'm always eager to tackle new challenges head-on.</p>
+
+<p className='mb-3'>I firmly believe that the key to success lies in consistent effort and a growth-oriented mindset. I'm not content with merely meeting expectations; I strive to exceed them at every opportunity. Each line of code I write is a testament to my dedication to excellence and my unwavering commitment to self-improvement.</p>
+
+<p className='mb-3'>From a young age, I've been fascinated by technology and its potential to shape the world. I'm driven by a passion for innovation and a desire to create meaningful solutions that enhance user experiences and solve real-world problems.</p>
+
+<p className='mb-3'>I've invested time in honing my English language skills, recognizing its importance in the tech industry. While my focus is on web development, I remain open to exploring other languages and expanding my linguistic horizons.</p>
+
+<p className='mb-3'>Outside of coding, I have a diverse range of interests. I'm an avid sports enthusiast, particularly enjoying activities such as running and hiking. Reading is another passion of mine, allowing me to unwind and explore new ideas.</p>
+
+<p className='mb-3'>I'm fully dedicated to honing my skills in the MERN stack and frontend development, aiming to create polished and impactful web experiences. My goal is to leverage my knowledge and creativity to build innovative solutions that resonate with users.</p>
+
+        </div>
         </div>
     )
 }
