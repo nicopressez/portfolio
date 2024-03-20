77b6f22f5025422bfc59f1649bc341687aa6c870
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-03-20 16:41:11 +0100

    Added PDF resume & download link

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,22 +1,55 @@
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Document, Page, pdfjs } from 'react-pdf';
+import { useState } from 'react';
+import NicolasPressezResume2 from './assets/NicolasPressezResume2.pdf'
+
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
 
 const Resume = ( { setShowResume }) => {
+
+        const [numPages, setNumPages] = useState(1);
+    
+        
+      
     return (
         
         <div
-        className={`bg-gray-200 w-2/3 absolute top-1 left-1/2 h-full transform -translate-x-1/2 z-50 rounded-xl font-roobertreg text-black p-12 overflow-y-auto transition-transform
+        className={`bg-white w-2/3 absolute top-1 left-1/2 h-[60rem] transform -translate-x-1/2 z-50 rounded-xl font-roobertreg text-black p-12 overflow-hidden transition-transform
         
          `}
-         style={{ overflowY: "scroll", scrollbarWidth: "none", msOverflowStyle: "none" }}
+         style={{
+            overflowY: "scroll",
+            scrollbarWidth: "none",
+            msOverflowStyle: "none",
+          }}
+        
       >
         <FontAwesomeIcon icon={faXmark} 
             className="float-right h-6 w-6 hover:cursor-pointer"
             onClick={ () => setShowResume(false)}/>
             <h1 className="text-4xl">Resume</h1>
-            <p className="text-4xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, quis fugit vitae repellendus itaque iusto, quae illo at dolorum facere delectus fuga magni, laborum assumenda sed. Mollitia quibusdam sequi iusto?</p>
+            <a
+        href={NicolasPressezResume2}
+        download="NicolasPressezResume2.pdf"
+        className=" absolute right-10 top-16 mt-4 text-blue-500 hover:underline"
+      >
+        Download resume
+      </a>
+            <div className="w-30  ml-52 mr-52 mt-5">
             
+            <Document
+            className="w-full"
+        file={NicolasPressezResume2}
+      >
+        <Page pageNumber={1} 
+        scale={1.3}/>
+      </Document>
+      </div>
         </div>
     )
 }
